2c3e43bed18fba34fbd2f58d8209d53717a12470
farha-neu <farhajw@gmail.com>
2018-07-12 17:32:23 -0700

    removed comments

--- server.js
+++ server.js
@@ -3,10 +3,6 @@ var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 var path = require("path");
-
-// Our scraping tools
-// Axios is a promised-based http library, similar to jQuery's Ajax method
-// It works on the client and on the server
 var axios = require("axios");
 var cheerio = require("cheerio");
 
@@ -15,20 +11,14 @@ var db = require("./models");
 
 var PORT = process.env.PORT || 3000;
 
-// Initialize Express
 var app = express();
-
-// Configure middleware
-
-// Use morgan logger for logging requests
+ 
 app.use(logger("dev"));
-// Use body-parser for handling form submissions
 app.use(bodyParser.urlencoded({ extended: true }));
-// Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-var databaseUri = "mongodb://localhost/scrapperDb";
+var databaseUri = "mongodb://localhost/scraperDb";
 if(process.env.MONGODB_URI){
   mongoose.connect(process.env.MONGODB_URI);
 }
@@ -46,33 +36,22 @@ dbConnect.once('open',function(){
 })
 
 
-// Routes
-
-// A GET route for scraping the echoJS website
 app.get("/scrape", function(req, res) {
-  // First, we grab the body of the html with request
   axios.get("https://www.washingtonpost.com/food/").then(function(response) {
-    // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
-
-    // Now, we grab every h2 within an article tag, and do the following:
     $(".story-list-story").each(function(i, element) {
-      // Save an empty result object
       var result = {};
       var newsLink = $(element).find(".story-image").find("a").attr("href");
-       var imgLink = $(element).find(".story-image").find("a").find("img").attr("data-hi-res-src");
+      var imgLink = $(element).find(".story-image").find("a").find("img").attr("data-hi-res-src");
       var headLine =  $(element).find(".story-headline").find("h3").text();
       var summary =  $(element).find(".story-description").find("p").text();
-      // Add the text and href of every link, and save them as properties of the result object
+     
       result.title = headLine;
       result.link = newsLink;
       result.image = imgLink;
       result.summary = summary;
-
-      // Create a new Article using the `result` object built from scraping
       db.Article.create(result)
         .then(function(dbArticle) {
-          // View the added result in the console
           console.log(dbArticle);
         })
         .catch(function(err) {
@@ -81,63 +60,48 @@ app.get("/scrape", function(req, res) {
           console.log(err);
         });
     });
-
-    // If we were able to successfully scrape and save an Article, send a message to the client
     res.send("Scrape Complete");
   });
 });
 
 // Route for getting all Articles from the db
 app.get("/articles/:saved", function(req, res) {
-  // Grab every document in the Articles collection
   db.Article.find({isSaved:req.params.saved}).populate("note")
     .then(function(dbArticle) {
-      // If we were able to successfully find Articles, send them back to the client
       res.json(dbArticle);
     })
     .catch(function(err) {
-      // If an error occurred, send it to the client
       res.json(err);
     });
 });
 
 // Route for grabbing a specific Article by id, populate it with it's note
 app.get("/article/:id", function(req, res) {
-  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
   db.Article.findOne({ _id: req.params.id })
-    // ..and populate all of the notes associated with it
     .populate("note")
     .then(function(dbArticle) {
-      // If we were able to successfully find an Article with the given id, send it back to the client
       res.json(dbArticle);
     })
     .catch(function(err) {
-      // If an error occurred, send it to the client
       res.json(err);
     });
 });
 
 // Route for saving/updating an Article's associated Note
 app.post("/articles/:id", function(req, res) {
-  // Create a new note and pass the req.body to the entry
   db.Note.create(req.body)
     .then(function(dbNote) {
-      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
       return db.Article.findOneAndUpdate({ _id: req.params.id },{$push: {note: dbNote._id} }, { new: true });
     })
     .then(function(dbArticle) {
-      // If we were able to successfully update an Article, send it back to the client
       res.json(dbArticle);
     })
     .catch(function(err) {
-      // If an error occurred, send it to the client
       res.json(err);
     });
 });
 
-//updating articles to saved/unsaved
+//saving/unsaving articles
 app.put("/article/:id/saved/:saved",function(req,res){
   db.Article.update({_id:req.params.id},{$set:{isSaved:req.params.saved}},{ new: true }).then(function(dbArticle){
     res.json(dbArticle);
@@ -149,14 +113,11 @@ app.put("/article/:id/saved/:saved",function(req,res){
 
 app.delete("/article/:id/note/:noteId",function(req,res){
   db.Note.findByIdAndRemove(req.params.noteId).then(function(){
-      console.log("--------------------");
       return db.Article.update({ _id: req.params.id }, {$pull: {note:req.params.noteId}});
   }).then(function(dbArticle) {
-    // If we were able to successfully update an Article, send it back to the client
     res.json(dbArticle);
   })
   .catch(function(err) {
-    // If an error occurred, send it to the client
     res.json(err);
   });
 });
@@ -175,6 +136,7 @@ app.delete("/delete",function(req,res){
 app.get("/saved", function(req, res) {
   res.sendFile(path.join(__dirname, "public/saved.html"));
 });
+
 // Start the server
 app.listen(PORT, function() {
   console.log("App running on port " + PORT + "!");
